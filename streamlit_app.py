142e49a0f717ad8b82ef0ef4ded3477b25a6efc5
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 13:34:39 +0300

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,27 @@ from random import shuffle
 streamlit.title('🎮 Edvins Game 🎮')
 
 
-# mylist =["x", "x", 1]
-# play = input("will you play the game?: ")
-# def shuffle_list(mylist):
-#     shuffle(mylist)
-#     return mylist
+mylist =["x", "x", 1]
+play = input("will you play the game?: ")
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
 
-# shuffle_list(mylist)
-# def game():
+shuffle_list(mylist)
+def game():
     
-#     if play == "yes":
-#         print("[ x | x | x ]")
-#         number = int(input("Chose and write number from 1 to 3, wich 'x' from this table have number '1': "))
-#         if number < 1 or number > 3:
-#             print("Please next time write correct numbers!")
-#         elif number >= 1 and number <=3:
-#             if mylist[number-1] == 1:
-#                 print("you win!")
-#                 print(mylist)
-#             else:
-#                 print("Wrong!!")
-#                 print(mylist)
-#     else:
-#         print("okay, see you next time")
-# game()
+    if play == "yes":
+        print("[ x | x | x ]")
+        number = int(input("Chose and write number from 1 to 3, wich 'x' from this table have number '1': "))
+        if number < 1 or number > 3:
+            print("Please next time write correct numbers!")
+        elif number >= 1 and number <=3:
+            if mylist[number-1] == 1:
+                print("you win!")
+                print(mylist)
+            else:
+                print("Wrong!!")
+                print(mylist)
+    else:
+        print("okay, see you next time")
+game()
