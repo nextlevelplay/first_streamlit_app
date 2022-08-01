c1f5dcaa4a477840b666be97dd7341ee0b475606
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 14:21:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit = my_fruit.set_index('Fruit')
 # Let's put some choose availability to customers
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) # pre-selected fruits
@@ -21,4 +21,4 @@ streamlit.multiselect("pick some fruits:",list(my_fruit.index),['Avocado','Straw
 
 
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit)
