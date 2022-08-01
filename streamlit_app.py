f12c310d618683d4d28bec2817f23736bd82e104
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 12:39:41 +0300

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put some choose availability to customers
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
