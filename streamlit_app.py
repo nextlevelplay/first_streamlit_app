5f64272795b9cc29c098e4a95477bb1feca00b5f
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 14:40:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) # pre-selected fruits
 
-fruit_to_show = my_fruit_list.loc[fruit_selected],columns=['Serving_Size', 'Calories']
+fruit_to_show = my_fruit_list.loc[fruit_selected]
 
-streamlit.dataframe(fruit_to_show)
+streamlit.dataframe(fruit_to_show, columns=['Serving_Size', 'Calories'])
