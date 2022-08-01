34f5757a9a57c05793ca9c478afc1e390686a84f
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 13:31:16 +0300

    Update streamlit_app.py
    start

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,30 @@
+import streamlit
 import shuffle
 
-mylist =["x", "x", 1]
-play = input("will you play the game?: ")
-def shuffle_list(mylist):
-    shuffle(mylist)
-    return mylist
+steramlit.title('🎮 Edvins Game 🎮')
 
-shuffle_list(mylist)
-def game():
+
+# mylist =["x", "x", 1]
+# play = input("will you play the game?: ")
+# def shuffle_list(mylist):
+#     shuffle(mylist)
+#     return mylist
+
+# shuffle_list(mylist)
+# def game():
     
-    if play == "yes":
-        print("[ x | x | x ]")
-        number = int(input("Chose and write number from 1 to 3, wich 'x' from this table have number '1': "))
-        if number < 1 or number > 3:
-            print("Please next time write correct numbers!")
-        elif number >= 1 and number <=3:
-            if mylist[number-1] == 1:
-                print("you win!")
-                print(mylist)
-            else:
-                print("Wrong!!")
-                print(mylist)
-    else:
-        print("okay, see you next time")
-game()
+#     if play == "yes":
+#         print("[ x | x | x ]")
+#         number = int(input("Chose and write number from 1 to 3, wich 'x' from this table have number '1': "))
+#         if number < 1 or number > 3:
+#             print("Please next time write correct numbers!")
+#         elif number >= 1 and number <=3:
+#             if mylist[number-1] == 1:
+#                 print("you win!")
+#                 print(mylist)
+#             else:
+#                 print("Wrong!!")
+#                 print(mylist)
+#     else:
+#         print("okay, see you next time")
+# game()
