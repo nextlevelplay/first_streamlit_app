425e8a162d9fd2ba39851b402c64ab2501a33443
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-01 14:19:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,11 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put some choose availability to customers
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) # pre-selected fruits
-fruit_to_show = my_fruit_list.loc[fruit_selected]
+# fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) # pre-selected fruits
+# fruit_to_show = my_fruit_list.loc[fruit_selected]
+
+streamlit.multiselect("pick some fruits:",list(my_fruit.index),['Avocado','Straw berries'])
 
 
 
-streamlit.dataframe(fruit_to_show)
+streamlit.dataframe(my_fruit_list)
