eedf6ad731603fc6d25af0fb0940970314d08874
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-04 12:17:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_load_list = my_fruit_list.set_index('Fruit')
 # Let's put some choose availability to customers
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) # pre-selected fruits
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_load_list.index),['Avocado','Strawberries']) # pre-selected fruits
 
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 
@@ -40,7 +40,7 @@ streamlit.dataframe(fruityvice_normal_view)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruityvice_normal_view")
+my_cur.execute("select * from my_fruit_load_list")
 my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
