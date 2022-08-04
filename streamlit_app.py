1afaf0ff0747d3c62a005eca8913f3dd32e2ffe9
Pavels Fomins <80169865+nextlevelplay@users.noreply.github.com>
2022-08-04 11:53:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import pandas
 import requests
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title('🎮 Edvins Game 🎮')
 answers_list = ["Yes", "No"]
 answer_field = streamlit.multiselect("Will you play the game? Make a choice (yes / no): ", list(answers_list))
